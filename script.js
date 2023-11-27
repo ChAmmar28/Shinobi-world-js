b1a54736cb3be4a267976c93c3466d87100ac389
ChAmmar28 <niyazbekovammar@gmail.com>
2023-11-27 18:50:00 +0600

    1st comit Ammar

---
+++ script.js
@@ -0,0 +1,258 @@
+// const text = document.querySelector(".someText");
+
+// // let word = prompt("write sth");
+
+// // text.innerHTML = word;
+
+// let fromUser = prompt("text");
+
+// let newText = `<span class="adil"> ${fromUser} </span>`;
+
+// text.insertAdjacentHTML("beforebegin", newText);  новый текст => HTML
+
+// 'beforebegin' : до самого элемента (до открывающего тега)
+
+// 'afterbegin' : после открывающего тега ( в начале )
+
+// 'beforeend' :  перед закрывающим тегом ( в конце )
+
+// 'afterend ': после самого элемента (после закрывающего тега)
+
+//
+
+// const someText = document.querySelector(".someText");
+
+// const arrOfGroup = ["Ammar", "Sultan", "Elaman", "Alihan", "Dilbar", "Sarynji"];
+
+// function cardGener(arr) {
+//   const res = arr.map((item) => `<span class="adil"> ${item} </span>`).join("");
+//   return res;
+// }
+
+// someText.insertAdjacentHTML("afterbegin", cardGener(arrOfGroup));
+
+//
+
+let shinobiWorld = [
+  {
+    name: "Naruto",
+    clan: "Uzumaki",
+    gender: "Male",
+    rang: "Genin",
+    vallage: "Leafy Village",
+    image:
+      "https://i0.wp.com/www.smartprix.com/bytes/wp-content/uploads/2022/11/Naruto.jpg?fit=1280%2C720&ssl=1",
+  },
+  {
+    name: "Saske",
+    clan: "Uchiha",
+    gender: "Male",
+    rang: "Genin",
+    vallage: "Leafy Village",
+    image:
+      "https://media.comicbook.com/2021/03/boruto-naruto-sasuke-rinnegan-1259450.jpeg?auto=webp",
+  },
+  {
+    name: "Sakura",
+    clan: "Haruno",
+    gender: "Female",
+    rang: "Jonin",
+    vallage: "Leafy Village",
+    image:
+      "https://sportshub.cbsistatic.com/i/2021/04/08/bb391e78-ad23-493c-a5c6-a83db4daad30/happybirthdysakura-987070.jpg",
+  },
+  {
+    name: "Kakashi",
+    clan: "hatake",
+    gender: "Male",
+    rang: "Sennin",
+    vallage: "Leafy Village",
+    image:
+      "https://static1.cbrimages.com/wordpress/wp-content/uploads/2023/04/naruto-kakashi-s-10-best-quotes.jpg",
+  },
+  {
+    name: "Orochimaru",
+    clan: "",
+    gender: "Male",
+    rang: "Sanin",
+    vallage: "Leafy Village",
+    image:
+      "https://static.wikia.nocookie.net/naruto/images/1/14/Orochimaru_Infobox.png/revision/latest?cb=20151017160235&path-prefix=ru",
+  },
+  {
+    name: "Madara",
+    clan: "Uchiha",
+    gender: "Male",
+    rang: "dancer(died)",
+    vallage: "Leafy Village",
+    image:
+      "https://sun9-56.userapi.com/impf/c850124/v850124490/cbafc/pqpDEyxae-Y.jpg?size=807x533&quality=96&sign=925f8a5abd5f8f0cd885e09ce61f7323&type=album",
+  },
+  {
+    name: "Gaara",
+    clan: "",
+    gender: "Male",
+    rang: "Kazekage",
+    vallage: "Sand Village",
+    image:
+      "https://static.wikia.nocookie.net/naruto/images/0/0f/Gaara_Part_II.png/revision/latest?cb=20170507172824&path-prefix=ru",
+  },
+  {
+    name: "Yamato",
+    clan: "",
+    gender: "Male",
+    rang: "Jonin",
+    vallage: "Leafy Village",
+    image:
+      "https://static.wikia.nocookie.net/naruto/images/f/f7/Yamato_newshot.png/revision/latest?cb=20190202094154&path-prefix=ru",
+  },
+  {
+    name: "Jiraya",
+    clan: "",
+    gender: "Male",
+    rang: "Sanin (died)",
+    vallage: "Leafy Village",
+    image:
+      "https://static.wikia.nocookie.net/naruto/images/2/21/Profile_Jiraiya.PNG/revision/latest?cb=20170818131513&path-prefix=ru",
+  },
+  {
+    name: "Guy",
+    clan: "Maito",
+    gender: "Male",
+    rang: "Jonin",
+    vallage: "Leafy Village",
+    image:
+      "https://static.wikia.nocookie.net/naruto/images/3/31/Might_Guy.png/revision/latest/scale-to-width-down/300?cb=20150401084456",
+  },
+  {
+    name: "Sunade",
+    clan: "Senju",
+    gender: "Female",
+    rang: "Sanin",
+    vallage: "Leafy Village",
+    image:
+      "https://prompthero.com/rails/active_storage/blobs/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaEpJaWsxTURVNVl6WTNaaTFoTUdFNUxUUTFZbVV0WVRVMk5DMDNNemt6WmpCaE9EazVZekFHT2daRlZBPT0iLCJleHAiOm51bGwsInB1ciI6ImJsb2JfaWQifX0=--de439eff1169c9dde8a2788ea5aa790b797494dc/136028.jpeg",
+  },
+  {
+    name: "Hirusen",
+    clan: "Sarutobi",
+    gender: "Male",
+    rang: "Hockage (died)",
+    vallage: "Leafy Village",
+    image:
+      "https://static.wikia.nocookie.net/naruto/images/e/e4/Hiruzen_Sarutobi.png/revision/latest?cb=20170930091606&path-prefix=ru",
+  },
+  {
+    name: "Tobirama",
+    clan: "Senju",
+    gender: "Male",
+    rang: "Hockage (died)",
+    vallage: "Leafy Village",
+    image:
+      "https://static.wikia.nocookie.net/naruto/images/b/be/Tobirama_Senju.png/revision/latest?cb=20200325223703&path-prefix=ru",
+  },
+  {
+    name: "Killer",
+    clan: "Be",
+    gender: "Male",
+    rang: "Jinchuriki",
+    vallage: "Clouds Village",
+    image:
+      "https://static.wikia.nocookie.net/character-power/images/f/fa/%D0%9A%D0%B8%D0%BB%D0%BB%D0%B5%D1%80_%D0%91%D0%B81.png/revision/latest?cb=20140326105611&path-prefix=ru",
+  },
+  {
+    name: "Minato",
+    clan: "Namikaze",
+    gender: "Male",
+    rang: "Hockage(died)",
+    vallage: "Leafy Village",
+    image:
+      "https://static.wikia.nocookie.net/naruto/images/7/71/Minato_Namikaze.png/revision/latest?cb=20200324174322&path-prefix=ru",
+  },
+  {
+    name: "Danzo",
+    clan: "Shimuro",
+    gender: "Male",
+    rang: "Hockage(died)",
+    vallage: "Leafy Village",
+    image:
+      "https://static.wikia.nocookie.net/naruto/images/0/08/Danzo_Shimura.png/revision/latest?cb=20181230092441&path-prefix=ru",
+  },
+  {
+    name: "Onoki",
+    clan: "",
+    gender: "Male",
+    rang: "Suchikage",
+    vallage: "Rock Village",
+    image:
+      "https://static.wikia.nocookie.net/naruto/images/6/67/%C5%8Cnoki.png/revision/latest?cb=20200328000054&path-prefix=ru",
+  },
+  {
+    name: "Choji",
+    clan: "Akimichi",
+    gender: "Male",
+    rang: "lonin",
+    vallage: "Leafy Village",
+    image:
+      "https://static.wikia.nocookie.net/naruto/images/e/ec/Ch%C5%8Dji_Part_II.png/revision/latest?cb=20141205195635&path-prefix=ru",
+  },
+  {
+    name: "Deydara",
+    clan: "",
+    gender: "Male",
+    rang: "Bomber(died)",
+    vallage: "Rock Village",
+    image:
+      "https://static.wikia.nocookie.net/naruto/images/d/d3/Deidara_mugshot.png/revision/latest/top-crop/width/360/height/450?cb=20170203125226&path-prefix=ru",
+  },
+  {
+    name: "Hinata",
+    clan: "Huiga",
+    gender: "Female",
+    rang: "Jonin",
+    vallage: "Leafy Village",
+    image:
+      "https://i.pinimg.com/originals/63/91/be/6391be5cac6f10c57c9a5eaa83a7b473.jpg",
+  },
+  {
+    name: "Ino",
+    clan: "Yamanako",
+    gender: "Female",
+    rang: "Jonin",
+    vallage: "Leafy Village",
+    image:
+      "https://static.wikia.nocookie.net/naruto/images/0/0c/Ino_Part_III.png/revision/latest?cb=20201206113822&path-prefix=ru",
+  },
+  {
+    name: "Itachi",
+    clan: "Uchiha",
+    gender: "Male",
+    rang: "Killer(programmer(died))",
+    vallage: "Leafy Village",
+    image:
+      "https://static.wikia.nocookie.net/naruto/images/b/bb/Itachi.png/revision/latest?cb=20210225204731&path-prefix=ru",
+  },
+];
+const $container = document.querySelector(".container");
+
+function cardGener(arr) {
+  const res = arr
+    .map(
+      (item) =>
+        `
+      <div class="card">
+        <img src='${item.image} alt='carecter' class="img"> 
+        <div class="cardText">
+            <h3>${item.name}</h3>
+            <h4>${item.rang}</h4>
+            <h4>${item.vallage}</h4>
+            <h5>${item.gender}</h5>
+        </div>
+      </div>
+      `
+    )
+    .join("");
+  return res;
+}
+
+$container.insertAdjacentHTML("afterbegin", cardGener(shinobiWorld));
